a98d85542872a45a5b4ec5830e62b48975f271d2
strongerDeer <stronger_deer@naver.com>
2023-10-23 20:05:09 +0900

    📝 Edit: link 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,8 @@ export default function Header() {
       </h1>
 
       <nav>
-        {/* <Link to="/login">Login</Link>
-    <Link to="/signup">Signup</Link> */}
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
         <Link to="/post/new">Write</Link>
         <Link to="/profile">Profile</Link>
         <Link to="/post">Posts</Link>
