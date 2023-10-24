ed759c36c68d50c1d1024c4c048f1acd75e0827a
strongerDeer <stronger_deer@naver.com>
2023-10-24 20:25:34 +0900

    ✨ Feat: 로그아웃

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,29 @@
+import { getAuth, signOut } from 'firebase/auth';
+import { app } from 'firebaseApp';
+
 import PostList from './PostList';
+import { toast } from 'react-toastify';
 
 export default function Profile() {
+  const auth = getAuth(app);
+
+  const onSignOut = async () => {
+    try {
+      await signOut(auth);
+      toast.success('로그아웃 되었습니다!');
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.code);
+    }
+  };
   return (
     <div className="profile__section">
       <img src="" alt="" />
-      <p>abc@gmail.com</p>
-      <p>홍길동</p>
-      <button type="button">로그아웃</button>
+      <p>{auth?.currentUser?.email}</p>
+      <p>{auth?.currentUser?.displayName || '사용자'}</p>
+      <button onClick={onSignOut} type="button">
+        로그아웃
+      </button>
       <PostList hasNavigation={false} />
     </div>
   );
