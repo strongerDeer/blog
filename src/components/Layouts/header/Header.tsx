1a2aa2a94414462f3d08a94f7c55b2589220a88c
strongerDeer <stronger_deer@naver.com>
2023-12-29 22:52:51 +0900

    📝 Edit: 메뉴 삭제 -> 메인 탭 처리 예정

--- src/components/Layouts/header/Header.tsx
+++ src/components/Layouts/header/Header.tsx
@@ -11,11 +11,6 @@ import SVGWrite from 'components/commons/SVG/SVGWrite';
 import SVGSignin from 'components/commons/SVG/SVGSignin';
 import SVGSignup from 'components/commons/SVG/SVGSignup';
 
-import { useContext } from 'react';
-import AuthContext from 'contexts/AuthContext';
-
-import useTranslation from 'hooks/useTranslation';
-
 import SettingModal from '../modal/SettingModal';
 import ProfileModal from '../modal/ProfileModal';
 import NotificationModal from '../modal/NotificationModal';
@@ -25,10 +20,6 @@ type HeadertProps = {
 };
 
 export default function Header({ isAuthenticated }: HeadertProps) {
-  const { user } = useContext(AuthContext);
-
-  const translation = useTranslation();
-
   return (
     <header className={styles.header}>
       <h1>
@@ -37,11 +28,6 @@ export default function Header({ isAuthenticated }: HeadertProps) {
           <span className="a11y-hidden">My Blog</span>
         </Link>
       </h1>
-
-      <nav>
-        <Link to="/post">{translation('MENU_POST')}</Link>
-        <Link to="/followingpost">팔로우중인 게시글(홈)</Link>
-      </nav>
 
       <div className={styles.btnGroup}>
         {isAuthenticated ? (
