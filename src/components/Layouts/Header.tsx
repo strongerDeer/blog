8b10bcee822f6fface058312bf84edd0c45c04dc
strongerDeer <stronger_deer@naver.com>
2024-01-06 21:37:42 +0900

    💄 UI: Headr UI 작업

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,9 +1,61 @@
+import styles from './Header.module.scss';
+
 import { Link } from 'react-router-dom';
 
+import Btn from 'components/commons/button/Btn';
+import SVGLogo from 'components/svg/SVGLogo';
+import SVGSignin from 'components/svg/SVGSignin';
+import SVGSignup from 'components/svg/SVGSignup';
+import SVGWrite from 'components/svg/SVGWrite';
+import SVGNotification from 'components/svg/SVGNotification';
+import SVGSetting from 'components/svg/SVGSetting';
+
 export default function Header() {
+  // const isAuthenticated = false;
+  const isAuthenticated = true;
   return (
-    <header>
-      <h1>My Blog</h1>
+    <header className={styles.header}>
+      <h1>
+        <Link to="/">
+          <SVGLogo />
+          <span className="a11y-hidden">My Blog</span>
+        </Link>
+      </h1>
+
+      <nav>
+        <Btn href="/post/create" fillPrimary>
+          <SVGWrite fill="gray00" />
+          Write
+        </Btn>
+        {isAuthenticated ? (
+          <>
+            {/* 로그인 */}
+            <button type="button">프로필</button>
+
+            <button type="button">
+              <SVGNotification />
+              <span className="a11y-hidden">알림</span>
+            </button>
+          </>
+        ) : (
+          <>
+            {/* 미로그인 */}
+            <Btn href="/login">
+              <SVGSignin fill="primary" />
+              Login
+            </Btn>
+            <Btn href="/signup">
+              <SVGSignup />
+              SignUp
+            </Btn>
+          </>
+        )}
+
+        <button type="button">
+          <SVGSetting />
+          <span className="a11y-hidden">설정</span>
+        </button>
+      </nav>
     </header>
   );
 }
---
+++ src/components/commons/button/Btn.tsx
@@ -0,0 +1,38 @@
+import styles from './Btn.module.scss';
+
+import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+interface BtnProps {
+  children?: ReactNode;
+  href?: string;
+  type?: 'button' | 'submit' | 'reset';
+  className?: string;
+  fillPrimary?: boolean;
+  disabled?: boolean;
+}
+
+export default function Btn(props: BtnProps) {
+  const { children, href, type, className, fillPrimary, ...rest } = props;
+
+  const classList = classNames(
+    className,
+    styles.btn,
+    fillPrimary ? styles.fillPrimary : null,
+  );
+
+  if (href) {
+    return (
+      <Link className={classList} to={href}>
+        {children}
+      </Link>
+    );
+  } else {
+    return (
+      <button className={classList} type={type || 'button'} {...rest}>
+        {children}
+      </button>
+    );
+  }
+}
