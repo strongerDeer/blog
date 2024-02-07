8f8add56a61b45be63e7b9460a5fd42815ab0a23
strongerDeer <stronger_deer@naver.com>
2024-02-07 23:11:22 +0900

    🐛 Fix: 스토리지 경로 이미지일 경우만 이미지 삭제

--- src/components/commons/button/DeletePostBtn.tsx
+++ src/components/commons/button/DeletePostBtn.tsx
@@ -9,19 +9,25 @@ import { toast } from 'react-toastify';
 import { deleteObject, ref } from 'firebase/storage';
 import { PostInterface } from 'interface';
 import SVGDelete from 'components/svg/SVGDelete';
+import { STORAGE_DOWNLOAD_URL_STR } from 'constants/index';
 
 export const postDelete = async (post: PostInterface) => {
   const { id, imgUrl } = post;
+
   const confirm = window.confirm('해당 게시글을 삭제하시겠습니까?');
+
   if (confirm && id) {
     // 이미지 먼저 삭제
-    const imageRef = ref(storage, imgUrl);
+    if (imgUrl.includes(STORAGE_DOWNLOAD_URL_STR)) {
+      const imageRef = ref(storage, imgUrl);
 
-    if (imgUrl) {
-      deleteObject(imageRef).catch((error) => {
-        console.log(error);
-      });
+      if (imgUrl) {
+        deleteObject(imageRef).catch((error) => {
+          console.log(error);
+        });
+      }
     }
+
     // 내용 삭제
     await deleteDoc(doc(db, 'posts', id));
     toast.success('삭제되었습니다.');
