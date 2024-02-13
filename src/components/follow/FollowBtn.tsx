10341305c61e24bf7a19fe87691b61cf5f84a2b4
strongerDeer <stronger_deer@naver.com>
2024-02-13 22:49:41 +0900

    ✨ Feat: 팔로잉/팔로우 알림 생성/취소

--- src/components/follow/FollowBtn.tsx
+++ src/components/follow/FollowBtn.tsx
@@ -1,12 +1,6 @@
 import Btn from 'components/commons/button/Btn';
 
-import {
-  arrayRemove,
-  arrayUnion,
-  doc,
-  onSnapshot,
-  updateDoc,
-} from 'firebase/firestore';
+import { deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore';
 import { db } from 'firebaseApp';
 import useFindUser from 'hooks/useFindUser';
 import { UserDataInterface } from 'interface';
@@ -22,38 +16,46 @@ interface FollowBtnProps {
 }
 
 export default function FollowBtn({ loginId, profileId }: FollowBtnProps) {
-  const pathname = useLocation().pathname;
-
   const { findUser: loginUser } = useFindUser(loginId);
   const { findUser: profileUser } = useFindUser(profileId);
-  const [followers, setFollowers] = useState<string[]>([]);
+
+  const [isFollow, setIsFollow] = useState<boolean>(false);
+  const [followUid, setFollowUid] = useState<string | null>(null);
 
   const onFollow = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    const timeId = new Date().getTime() + '';
+
     try {
       if (loginId && profileId) {
         // 로그인한 사용자 : 팔로잉에 추가
-        const followingRef = doc(db, 'users', loginId);
-
-        await updateDoc(followingRef, {
-          followingList: arrayUnion({
-            uid: profileId,
-            displayName: profileUser?.displayName,
-            email: profileUser?.email,
-            photoURL: profileUser?.photoURL,
-          }),
+        await setDoc(doc(db, 'users', loginId, 'followings', profileId), {
+          uid: timeId,
+          displayName: profileUser?.displayName,
+          email: profileUser?.email,
+          photoURL: profileUser?.photoURL,
         });
 
         // 팔로우 당하는 사용자: 팔로우에 추가
-        const followerRef = doc(db, 'users', profileId);
-        await updateDoc(followerRef, {
-          followerList: arrayUnion({
-            uid: loginId,
-            displayName: loginUser?.displayName,
-            email: loginUser?.email,
-            photoURL: loginUser?.photoURL,
+        await setDoc(doc(db, 'users', profileId, 'followers', loginId), {
+          uid: timeId,
+          displayName: loginUser?.displayName,
+          email: loginUser?.email,
+          photoURL: loginUser?.photoURL,
+        });
+
+        // 팔로우 당하는 사용자: 알림생성
+        await setDoc(doc(db, 'users', profileId, 'notifications', timeId), {
+          createdAt: new Date()?.toLocaleDateString('ko', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
           }),
+          isRead: false,
+          url: `/profile/${loginId}`,
+          content: `"${loginUser?.displayName}"님이 나를 팔로우합니다!`,
         });
       }
+
       toast.success('팔로우 하였습니다');
     } catch (error) {
       console.log(error);
@@ -62,32 +64,20 @@ export default function FollowBtn({ loginId, profileId }: FollowBtnProps) {
 
   const onDeleteFollow = async (e: React.MouseEvent<HTMLButtonElement>) => {
     const confirm = window.confirm('팔로우를 취소할까요?');
-
     if (confirm) {
       try {
         if (loginId && profileId) {
-          // 로그인한 사용자 : 팔로잉에 삭제
-          const followingRef = doc(db, 'users', loginId);
-
-          await updateDoc(followingRef, {
-            followingList: arrayRemove({
-              uid: profileId,
-              displayName: profileUser?.displayName,
-              email: profileUser?.email,
-              photoURL: profileUser?.photoURL,
-            }),
-          });
+          // 로그인한 사용자 : 팔로잉에서 삭제
+          await deleteDoc(doc(db, `users/${loginId}/followings`, profileId));
 
           // 팔로우 당하는 사용자: 팔로우에 삭제
-          const followerRef = doc(db, 'users', profileId);
-          await updateDoc(followerRef, {
-            followerList: arrayRemove({
-              uid: loginId,
-              displayName: loginUser?.displayName,
-              email: loginUser?.email,
-              photoURL: loginUser?.photoURL,
-            }),
-          });
+          await deleteDoc(doc(db, `users/${profileId}/followers`, loginId));
+        }
+        // 알림 삭제
+        if (followUid) {
+          await deleteDoc(
+            doc(db, `users/${profileId}/notifications`, followUid),
+          );
         }
         toast.success('팔로우 취소하였습니다!');
       } catch (error) {
@@ -98,15 +88,11 @@ export default function FollowBtn({ loginId, profileId }: FollowBtnProps) {
 
   const getFollowers = useCallback(async () => {
     if (profileId) {
-      const ref = doc(db, 'users', profileId);
-
-      onSnapshot(ref, (doc) => {
-        setFollowers([]);
-        doc
-          ?.data()
-          ?.followerList?.map((data: any) =>
-            setFollowers((prev) => (prev ? [...prev, data?.uid] : [])),
-          );
+      const ref = doc(db, `users/${profileId}`, `followers/${loginId}`);
+      onSnapshot(ref, (docSnap) => {
+        console.log();
+        setIsFollow(docSnap.exists());
+        setFollowUid(docSnap.data()?.uid);
       });
     }
   }, []);
@@ -119,7 +105,7 @@ export default function FollowBtn({ loginId, profileId }: FollowBtnProps) {
 
   return (
     <>
-      {loginId && followers?.includes(loginId) ? (
+      {isFollow ? (
         <Btn onClick={onDeleteFollow}>팔로잉 취소</Btn>
       ) : (
         <Btn onClick={onFollow} fillPrimary>
